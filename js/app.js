5ca174e8654d158ff5f383481d44624d0975e650
jonathan-crepeau <jon.crepeau@gmail.com>
2022-10-09 17:20:22 -0700

    successful express server test for get request for homepage

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log('Fear is the mind killer.');
+// console.log('Fear is the mind killer.');
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('This that pink venom.')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}.`);
+})
